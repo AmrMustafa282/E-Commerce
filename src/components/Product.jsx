abfbbc976218749e6aef1e957f5ab36d98d7dd4f
Mhmd0Mhmod <iammhmdmhmod@gmail.com>
2024-05-03 12:23:32 +0300

    Try Product demo

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Images } from "@/lib/images";
+import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./ui/card";
 
-const Product = () => {
-  return <div></div>;
+const Product = ({ product }) => {
+  return (
+    <Card>
+      <CardHeader>
+        <img src={Images.product[0]} alt="product.name" />
+      </CardHeader>
+      <CardContent></CardContent>
+      <CardTitle>{product.name}</CardTitle>
+      <CardDescription>{product.disc}</CardDescription>
+      <CardFooter>Price: {product.price}</CardFooter>
+    </Card>
+  );
 };
 
 export default Product;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,30 @@
 import { HomeCarousel } from "@/components/Carousel";
-import React from "react";
-
+import Product from "@/components/Product";
+const products = [
+  {
+    id: 1,
+    name: "T-shirt",
+    disc: "Nike T-shirt       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur natus quia fugiat eveniet voluptatum, sed excepturi enim nobis vel quo, aperiam sit. Ad laboriosam magni labore reiciendis. Minima, accusantium! Dolores!",
+    price: "30$",
+  },
+  {
+    id: 2,
+    name: "Short",
+    disc: "Nike short         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur natus quia fugiat eveniet voluptatum, sed excepturi enim nobis vel quo, aperiam sit. Ad laboriosam magni labore reiciendis. Minima, accusantium! Dolores! ",
+    price: "20$",
+  },
+];
 const Home = () => {
- return (
-  <div className="w-full">
-   <HomeCarousel />
-  </div>
- );
+  return (
+    <div className="w-full">
+      <HomeCarousel />
+      <div className="grid grid-cols-4 mt-10 gap-10">
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
